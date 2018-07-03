1fea2419881bb83dd821eb50c5f29c1778e440d5
akbardhany <dhanydhuny@gmail.com>
2018-07-03 21:32:54 +0700

    ubah

--- connection.php
+++ connection.php
@@ -6,10 +6,4 @@
 
   $conn = mysqli_connect($servername,$username,$pass,$database);
 
-  if (!$conn) {
-    die("Connection Failed: ".mysqli_connect_error());
-  }
-    echo "Connected Successfully";
-
-  mysqli_close($conn);
  ?>
