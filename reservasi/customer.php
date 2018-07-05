ac3ae6bcf189194781c1ff7b385e1d1cc9f6b4fd
akbardhany <dhanydhuny@gmail.com>
2018-07-05 23:32:09 +0700

    form customer

---
+++ reservasi/customer.php
@@ -0,0 +1,40 @@
+<form class="" action="data-customer.php" method="post">
+  <table>
+    <tr>
+      <td>Customer Name</td>
+      <td>:</td>
+      <td><input type="text" name="customer-name" placeholder="Customer Name" required /></td>
+    </tr>
+    <tr>
+      <td>Customer ID</td>
+      <td>:</td>
+      <td><input type="number" name="customer-name" min="0" placeholder="16 digit" required /></td>
+    </tr>
+    <tr>
+      <td>Customer Address</td>
+      <td>:</td>
+      <td><input type="text" name="customer-address" placeholder="Customer Address" required /></td>
+    </tr>
+    <tr>
+      <td>Customer Phone</td>
+      <td>:</td>
+      <td><input type="tel" name="customer-phone" placeholder="+62" required /></td>
+    </tr>
+    <tr>
+      <td>Customer Email</td>
+      <td>:</td>
+      <td><input type="email" name="customer-email" placeholder="email@Customer.com" required /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td align="right"><input type="submit" name="submit" value="Submit"></td>
+    </tr>
+  </table>
+</form>
+<br />
