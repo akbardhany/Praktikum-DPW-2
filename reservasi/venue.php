7173ae3d2654174cb56335e65aa0307b8a81a5b1
akbardhany <dhanydhuny@gmail.com>
2018-07-05 23:33:26 +0700

    form venue

---
+++ reservasi/venue.php
@@ -0,0 +1,26 @@
+<form class="" action="data-venue.php" method="post">
+  <table>
+    <tr>
+      <td>Venue Name</td>
+      <td>:</td>
+      <td><input type="text" name="venue-name" placeholder="Venue Name" required /></td>
+    </tr>
+    <tr>
+      <td>Venue Address</td>
+      <td>:</td>
+      <td><input type="text" name="venue-address" placeholder="Venue Address" required /></td>
+    </tr>
+    <tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td align="right"><input type="submit" name="submit" value="Submit"></td>
+    </tr>
+  </table>
+</form>
+<br />
