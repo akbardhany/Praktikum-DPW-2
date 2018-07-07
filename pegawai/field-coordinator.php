f445df477023882a63ac2b3e03e431d748f381db
akbardhany <dhanydhuny@gmail.com>
2018-07-08 00:15:18 +0700

    form field coordinator

---
+++ pegawai/field-coordinator.php
@@ -0,0 +1,40 @@
+<form class="" action="./config/data-fieldcoordinator.php" method="post">
+  <table>
+    <tr>
+      <td>Field Coor. Name</td>
+      <td>:</td>
+      <td><input type="text" name="fieldcoor-name" placeholder="Field Coor. Name" required /></td>
+    </tr>
+    <tr>
+      <td>Field Coor. ID</td>
+      <td>:</td>
+      <td><input type="number" name="fieldcoor-id" min="0" placeholder="16 digit" required /></td>
+    </tr>
+    <tr>
+      <td>Field Coor. Address</td>
+      <td>:</td>
+      <td><input type="text" name="fieldcoor-address" placeholder="Field Coor. Address" required /></td>
+    </tr>
+    <tr>
+      <td>Field Coor. Phone</td>
+      <td>:</td>
+      <td><input type="tel" name="fieldcoor-phone" placeholder="+62" required /></td>
+    </tr>
+    <tr>
+      <td>Field Coor. Email</td>
+      <td>:</td>
+      <td><input type="email" name="fieldcoor-email" placeholder="email@FieldCoor.com" required /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td align="right"><input type="submit" name="submit" value="Submit"></td>
+    </tr>
+  </table>
+</form>
+<br />
