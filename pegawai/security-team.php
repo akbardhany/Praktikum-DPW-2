4f5003825fc3113ec42fecef5b3e6edbe4173cde
akbardhany <dhanydhuny@gmail.com>
2018-07-08 00:15:51 +0700

    form security team

---
+++ pegawai/security-team.php
@@ -0,0 +1,40 @@
+<form class="" action="./config/data-securityteam.php" method="post">
+  <table>
+    <tr>
+      <td>Name of Sec. Officer</td>
+      <td>:</td>
+      <td><input type="text" name="security-name" placeholder="Full Name" required /></td>
+    </tr>
+    <tr>
+      <td>ID of Sec. Officer</td>
+      <td>:</td>
+      <td><input type="number" name="security-id" min="0" placeholder="16 digit" required /></td>
+    </tr>
+    <tr>
+      <td>Address of Sec. Officer</td>
+      <td>:</td>
+      <td><input type="text" name="security-address" placeholder="Address" required /></td>
+    </tr>
+    <tr>
+      <td>Phone of Sec. Officer</td>
+      <td>:</td>
+      <td><input type="tel" name="security-phone" placeholder="+62" required /></td>
+    </tr>
+    <tr>
+      <td>Members for The Team</td>
+      <td>:</td>
+      <td><input type="number" name="security-member" min="0" placeholder="5" required /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td align="right"><input type="submit" name="submit" value="Submit"></td>
+    </tr>
+  </table>
+</form>
+<br />
