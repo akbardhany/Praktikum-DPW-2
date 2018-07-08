3128659e620c92b8dcec5f6a65cb104fea7c2781
akbardhany <dhanydhuny@gmail.com>
2018-07-09 01:19:00 +0700

    insert employee

---
+++ pegawai/config/data-employee.php
@@ -0,0 +1,24 @@
+<?php
+  require_once '../../connection.php';
+  $emName = $_POST['employee-name'];
+  $emId   = $_POST['employee-id'];
+  $emGen  = $_POST['employee-gender'];
+  $emPos  = $_POST['employee-position'];
+  $emAddr = $_POST['employee-address'];
+  $emPho  = $_POST['employee-phone'];
+  $emImg  = $_FILES['employee-img']['name'];
+  $msg    = "";
+
+  $targetfolder = "../profile-photo/".basename($emImg);
+
+  if ($emName == null && $emId == null && $emGen == null && $emPos == null && $emAddr == null && $emPho == null) {
+    echo '<tt>'."u haven't filled out the data".'</tt>';
+    header("refresh:1.5 ; ../../pegawai");
+  }else {
+  $de = mysqli_query($conn, "INSERT INTO pegawai (nama_pegawai, nik_pegawai, jk_pegawai, jabatan_pegawai, alamat_pegawai, notelp_pegawai, foto_pegawai, waktu_pembuatan) VALUES ('$emName','$emId','$emGen','$emPos','$emAddr','$emPho','$emImg',now())");
+  }
+  if ($de && move_uploaded_file($_FILES['employee-img']['tmp_name'], $targetfolder)) {
+    header("location: ../../pegawai/");
+  }
+  mysqli_close($conn);
+ ?>
