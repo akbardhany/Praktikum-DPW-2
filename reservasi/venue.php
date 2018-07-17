a05841106c07b099bff4afef07124c227f269f52
Cheexs <4cheexs@gmail.com>
2018-07-17 23:23:03 +0700

    tambah input: image

--- reservasi/venue.php
+++ reservasi/venue.php
@@ -1,4 +1,4 @@
-<form class="" action="./config/data-venue.php" method="post">
+<form class="" action="./config/data-venue.php" method="post" enctype="multipart/form-data">
   <table>
     <tr>
       <td>Venue Name</td>
@@ -11,6 +11,12 @@
       <td><input type="text" name="venue-address" placeholder="Venue Address" required /></td>
     </tr>
     <tr>
+      <td>Photo of The Venue</td>
+      <td>:</td>
+      <td>
+        <input type="file" name="venue-img" accept="image/*" required />
+      </td>
+    </tr>
     <tr>
       <td>&nbsp;</td>
       <td>&nbsp;</td>
