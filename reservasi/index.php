f15dd3c8ca3e1031be210d18d0b37cb2d7ecf24a
akbardhany <dhanydhuny@gmail.com>
2018-07-18 00:01:14 +0700

    include file: select

--- reservasi/index.php
+++ reservasi/index.php
@@ -66,11 +66,11 @@
             <div class="col-sm-12 tab-content">
               <div id="select-customer" class="container tab-pane fade">
                 <h3>Customer Data</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <?php include './config/select-customer.php'; ?>
               </div>
               <div id="select-vendor" class="container tab-pane fade">
                 <h3>Vendor Data</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <?php include './config/select-vendor.php'; ?>
               </div>
               <div id="select-reservation" class="container tab-pane fade">
                 <h3>Reservation Data</h3>
