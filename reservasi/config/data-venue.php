8f0f3b8caea1e17d6ab7040541e3e9c6c677570e
Cheexs <4cheexs@gmail.com>
2018-07-17 23:24:43 +0700

    update insert: img

--- reservasi/config/data-venue.php
+++ reservasi/config/data-venue.php
@@ -2,12 +2,15 @@
   require_once '../../connection.php';
   $vename = $_POST['venue-name'];
   $veaddr = $_POST['venue-address'];
+  $veImg  = $_FILES['venue-img']['name'];
+
+  $targetfolder = "../image/".basename($veImg);
 
   if ($vename == null && $veaddr == null) {
     echo '<tt>'."u haven't filled out the data".'</tt>';
     header("refresh:1.5 ; ../../reservasi");
   }else {
-  $dv = mysqli_query($conn, "INSERT INTO tempat_acara (nama_tempatAcara, alamat_tempatAcara, waktu_pembuatan) VALUES ('$vename','$veaddr',now())");
+  $dv = mysqli_query($conn, "INSERT INTO tempat_acara (nama_tempatAcara, alamat_tempatAcara, foto_tempatAcara, waktu_pembuatan) VALUES ('$vename','$veaddr','$veImg',now())");
   }
   if ($dv) {
     header("location: ../../reservasi/");
